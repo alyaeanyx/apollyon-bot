5b69111f648135897bcb865574df9303c3f401f7
alyaeanyx <alyaeanyx@mailbox.org>
2021-04-12 20:21:36 +0200

    add JSON feed support #2

---
+++ feedlib/json_feed.py
@@ -0,0 +1,32 @@
+import requests
+import yaml
+import json
+from feedlib import Feed
+
+config = yaml.safe_load(open("config.yaml"))
+
+
+class JSONFeed(Feed):
+    def __init__(self, name, description, url, text):
+        Feed.__init__(self, name, description)
+        self.url = url
+        self.text = text
+
+    def fetch_worksheets(self):
+        res = requests.get(self.url)
+        if res.status_code != 200:
+            raise RuntimeError("Error accessing JSON feed")
+        
+        items = json.loads(res.text)
+        sheets = []
+        for item in items:
+            sheets.append((item["title"], item["url"]))
+        return sheets
+
+    def get_updates(self):
+        updates = []
+        for name, link in self.fetch_worksheets():
+            if not self.already_sent(link):
+                updates.append(self.text.format(name=name, link="https://uebungen.physik.uni-heidelberg.de"+link))
+                self.register_as_sent(link)
+        return updates
